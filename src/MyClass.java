fac331122e79391834bea146f633557d0f0cceb6
iBleakNight <ibleaknight@gmail.com>
2019-11-01 17:24:40 +0800

    new files123

---
+++ src/MyClass.java
@@ -0,0 +1,9 @@
+// 一个简单的构造函数
+class MyClass {
+    int x;
+
+    // 以下是构造函数
+    MyClass() {
+        x = 10;
+    }
+}
---
+++ src/Second/MyClass.java
@@ -0,0 +1,11 @@
+package Second;
+
+// 一个简单的构造函数
+class MyClass {
+    int x;
+
+    // 以下是构造函数
+    MyClass(int i ) {
+        x = i;
+    }
+}
