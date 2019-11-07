72c62c61556e77503f642bbf2ad0c150a86b4999
iBleakNight <ibleaknight@gmail.com>
2019-11-07 14:36:06 +0800

    New Java Demo - 2019.11.07

---
+++ src/Applet/CheckerApplet.java
@@ -0,0 +1,12 @@
+//package Applet;
+//
+//import java.applet.*;
+//import java.awt.*;
+//public class CheckerApplet extends Applet
+//{
+//    int squareSize = 50;// 初始化默认大小
+//    public void init () {}
+//    private void parseSquareSize (String param) {}
+//    private Color parseColor (String param) {}
+//    public void paint (Graphics g) {}
+//}
